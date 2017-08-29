92507f68f0f4b0157cd7995a987f6a22d272d6b2
Nikolay Liemiekhov <nicklemster@gmail.com>
2017-08-30 02:47:19 +0300

    traffic_light lesson 9

---
+++ src/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by IlyaLitvinov on 10.04.17.
+ */
+import './styles.styl';
+
+/*
+class HelloWorld {
+    constructor() {
+        console.log('Hello w5olddrd');
+    }
+}*/
+
+import {traffic_light} from "./traffic_light/traffic_light";
+
+traffic_light('.traffic-light-root');
---
+++ src/traffic_light/traffic_light.js
@@ -0,0 +1,54 @@
+//import './traffic_light.styl';
+
+function traffic_light(selector) {
+  const widgetTemplate = `
+    <div class="lamp red-light" data-operator="lightRed"></div>
+    <div class="lamp yellow-light" data-operator="lightYellow"></div>
+    <div class="lamp green-light" data-operator="lightGreen"></div>
+    `;
+
+  const rootElement = document.querySelector(selector);
+
+  let btnRed;
+  let btnYellow;
+  let btnGreen;
+
+  function renderStatic() {
+    rootElement.innerHTML = widgetTemplate;
+
+    btnRed = rootElement.querySelector('.red-light');
+    btnYellow = rootElement.querySelector('.yellow-light');
+    btnGreen = rootElement.querySelector('.green-light');
+  }
+
+  function handleEvents() {
+    btnRed.addEventListener('click', lightLamp);
+    btnYellow.addEventListener('click', lightLamp);
+    btnGreen.addEventListener('click', lightLamp);
+  }
+
+  function lightLamp() {
+    switch (this.getAttribute('data-operator')) {
+      case 'lightRed':
+        this.style.backgroundColor = 'red';
+        btnYellow.style.backgroundColor = '';
+        btnGreen.style.backgroundColor = '';
+        break;
+      case 'lightYellow':
+        btnRed.style.backgroundColor = '';
+        this.style.backgroundColor = 'yellow';
+        btnGreen.style.backgroundColor = '';
+        break;
+      case 'lightGreen':
+        btnRed.style.backgroundColor = '';
+        btnYellow.style.backgroundColor = '';
+        this.style.backgroundColor = 'green';
+        break;
+    }
+  }
+
+  renderStatic();
+  handleEvents();
+}
+
+export {traffic_light}
